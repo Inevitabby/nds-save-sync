06e1ebf382e8e7e7975c000ead117c2a0b02ffa0
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2026-02-17 12:50:29 -0800

    doc: Update state machine plans to match model
    Fleshed out the conditions a bit more and made the explanations match
    the actual model.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -5,35 +5,36 @@ import 'package:nds_save_sync/providers/dashboard_controller.dart';
 
 // STATES
 // 
-// 1. Disconnected (Idle):
-//  - Condition: Initial state, no "last IP" recorded
+// 1. Idle (Disconnected):
+//  - Condition: Initial state, lastAddr is null
 //  - Hero: "Search for Device" (TODO wording)
 // 
 // 2. Scanning:
-//  - Condition: Previous button pressed, OR app started with a "last IP" recorded
-//  - Hero: Some pinging/loading indicator "Connecting to your DS..." (TODO this wording is misleading)
+//  - Condition: Button pressed OR app started with lastAddr not null
+//  - Hero: Some loading indicator "Connecting to your DS..." (TODO this wording is misleading)
 //  - Fail:
-//    - Condition: User doesn't have WiFi OR Hotspot (instafail)
-//    -            Or, we couldn't find the DS on WiFi or Hotspot subnet 
-//    - Hero: TODO Should there be two different fail messages? One that is a gentle like modal with a few bullet points or something like, "Couldn't find DS 1. Is your phone and DS on the same hotspot or WiFi? 2. If your DS's FTP app running? etc." And have like two buttons like, "Let me try again" or "Let me enter the IP manually"
+//    - Condition: User not on WiFi OR Hotspot (instafail)
+//    -            OR we couldn't find the DS on WiFi/Hotspot subnet 
+//    - Hero: TODO (Should there be two different fail messages? One that is a gentle like modal with a few bullet points or something like, "Couldn't find DS 1. Is your phone and DS on the same hotspot or WiFi? 2. If your DS's FTP app running? etc." And have like two buttons like, "Let me try again" or "Let me enter the IP manually")
 // 
-// 3. First-Time Connect (Path Picker):
-//  - Condition: Found the DS but have no saveDir
-//  - Hero: Slide-up modal showing FTP directory browser, user finds and picks folder
-//          At this point, we will update the LAST_IP
+// 3. Connected:
+//  - Condition: Connection established to DS.
+//  - Case A: First-Connection (saveDir is null)
+//    - Hero: Slide-up modal showing FTP directory browser, user finds and picks saveDir folder
+//  - Case B: Ready (saveDir not null)_
+//    - Hero: "Tap to Sync"
 // 
-// 4. Ready (Connected): 
-//  - Hero: "Tap to Sync"
+// 4. Working
+//  - Condition: Sync started
+//  - Hero: Some sort of live log/progress indicator. The *bulk* of time will be waiting on the crap link so this'll need to be good. (TODO Design this)
 // 
-// 5. Working
-//  - Hero: Some sort of live log. The *bulk* of time will be waiting on the crap link so a "Currently downloading" would be nice. (TODO: Design this a bit better)
-// 
-// 6. Success:
-//  - Hero: Green button
+// 5. Success:
+//  - Condition: Sync completed
+//  - Hero: TODO Some sort of success indicator and something telling the user to check their archive. i.e., "Swipe left to see saves" or something
 
 // OTHER LOCATIONS
 // - Left: Save Archive
-// - Right: Settings xor Debug (Version Information and Logs (?))
+// - Right: Settings xor Debug (Version Information and Logs (TODO Plan this side))
 
 class Dashboard extends ConsumerWidget {
   const Dashboard({super.key});
